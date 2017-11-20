1867b126d405ca3835956acc0b5e73ae66a81271
Eaq19 <eandresk.13@hotmail.com>
2017-11-19 22:05:26 -0500

    Ajuste basicos

---
+++ view/js/index.js
@@ -0,0 +1,37 @@
+var App = (function (app) {
+
+    var load = function () {
+      var table = $('#tblVariables').DataTable({
+        "scrollX": true
+      });
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      $( "#tabs" ).tabs();
+    };
+
+    function drawChart() {
+      var data = google.visualization.arrayToDataTable([
+        ['Year', 'Sales', 'Expenses'],
+        ['2004',  1000,      400],
+        ['2005',  1170,      460],
+        ['2006',  660,       1120],
+        ['2007',  1030,      540]
+      ]);
+
+      var options = {
+        title: 'Company Performance',
+        curveType: 'function',
+        legend: { position: 'bottom' }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+      chart.draw(data, options);
+    }
+
+    return {
+        load: load
+    };
+})(App);
+
+App.load();
